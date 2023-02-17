6d316f494d7f5274067466f41aa9e27c7aea229e
Lucas Vinicius <119816215+lucassvinn@users.noreply.github.com>
2023-02-16 21:02:15 -0300

    Update exemplo(01).c

--- exemplo(01).c
+++ exemplo(01).c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    int v[5];
-    float m;
+    int v[5], m;
 
     v[0] = 50;
-    v[1] = 77;
+    v[1] = 70;
     v[2] = 40;
     v[3] = 25;
     v[4] = 10;
@@ -13,5 +12,6 @@ int main() {
     m = (v[0] + v[1] + v[2] + v[3] + v[4]) / 5;
 
     printf("%f\n", m);
+    
     return 0;
 }
