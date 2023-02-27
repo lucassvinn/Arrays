fd6bf44a82000e6e8bfcfc99a0010ec040ee3eb0
Lucas Vinicius <119816215+lucassvinn@users.noreply.github.com>
2023-02-27 20:02:40 -0300

    Create main(03).c

---
+++ main(03).c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int valor[10], i;
+
+    for(i=0; i<10; i++)
+    {
+        printf("Qual o %do valor?\n ", i+1);
+        scanf("%d", &valor[i]);
+    }
+    printf("\nO ordem inversa dos valores entrados e:\n");
+
+    for(i=0; i<10; i++)
+        printf("%d\n", valor[9-i]);
+    return 0;
+}
